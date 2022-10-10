7f307ad00e20a852987d793d89e620751b60df0b
sopkk3 <jorge3anoro@gmail.com>
2022-10-10 11:36:42 +0200

    Move to nvim autocmd

--- nvim/lua/sopkk/options.lua
+++ nvim/lua/sopkk/options.lua
@@ -1,5 +1,3 @@
-local command = vim.cmd
-
 local options = {
   completeopt = { 'menu', 'menuone', 'noselect' },
   encoding = 'utf-8',
@@ -47,26 +45,26 @@ for k, v in pairs(options) do
   vim.opt[k] = v
 end
 
--- TBD: move to nvim function
-command 'autocmd BufWritePre * %s/\\s\\+$//e' -- Deletes trailing whitespace on save
--- vim-commentary
-command 'autocmd FileType c,cpp,cs,java        setlocal commentstring=//\\ %s'
-command 'autocmd FileType sql                  setlocal commentstring=--\\ %s'
-command 'autocmd FileType toml                 setlocal commentstring=#\\ %s'
-
 vim.filetype.add({
   pattern = {
     ['.*Jenkinsfile.*'] = 'groovy'
   }
 })
 
-vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+vim.api.nvim_create_autocmd({ 'BufWritePre' }, { command = '%s/\\s\\+$//e'})
+
+-- vim-commentary
+vim.api.nvim_create_autocmd({ 'FileType' }, { pattern = { 'c', 'cpp', 'cs', 'java' }, command = 'setlocal commentstring=//\\ %s'})
+vim.api.nvim_create_autocmd({ 'FileType' }, { pattern = 'sql', command = 'setlocal commentstring=--\\ %s'})
+vim.api.nvim_create_autocmd({ 'FileType' }, { pattern = 'toml', command = 'setlocal commentstring=#\\ %s'})
+
+vim.api.nvim_create_autocmd({ 'TextYankPost' }, {
   callback = function()
-    vim.highlight.on_yank { higroup = "Visual", timeout = 200 }
+    vim.highlight.on_yank { higroup = 'Visual', timeout = 300 }
   end,
 })
 
-local group = vim.api.nvim_create_augroup("CursorLineControl", { clear = true })
+local group = vim.api.nvim_create_augroup('CursorLineControl', { clear = true })
 local set_cursorline = function(event, value, pattern)
   vim.api.nvim_create_autocmd(event, {
     group = group,
@@ -76,6 +74,6 @@ local set_cursorline = function(event, value, pattern)
     end,
   })
 end
-set_cursorline("WinLeave", false)
-set_cursorline("WinEnter", true)
-set_cursorline("FileType", false, "TelescopePrompt")
+set_cursorline('WinLeave', false)
+set_cursorline('WinEnter', true)
+set_cursorline('FileType', false, 'TelescopePrompt')
