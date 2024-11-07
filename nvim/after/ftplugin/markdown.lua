2be9078bc5851baa4abe09057b6165d1d2ad5e45
sopkk3 <jorge3anoro@gmail.com>
2024-11-07 08:22:26 +0100

    Small path updates

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -1,4 +1,3 @@
-vim.opt_local.wrap = false
 local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<leader>WC', 'mx0f[lrx<Esc>`x', opts)
 vim.keymap.set('n', '<leader>WD', 'mx0f[fxr<Space><Esc>`x', opts)
