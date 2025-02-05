1bf21004831dd1324749458bbde1bbbbfcad2bf5
sopkk3 <jorge3anoro@gmail.com>
2025-02-05 20:06:38 +0100

    Remove nvim-cmp preselect

--- nvim/lua/sopkk/lazy/lsp.lua
+++ nvim/lua/sopkk/lazy/lsp.lua
@@ -97,6 +97,7 @@ return {
             require('luasnip').lsp_expand(args.body)
           end,
         },
+        preselect = cmp.PreselectMode.None,
         mapping = cmp.mapping.preset.insert({
           ['<C-d>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
