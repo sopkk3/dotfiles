d7d0926bcf7cd58cc69187864ce66e4c142afa10
sopkk3 <jorge3anoro@gmail.com>
2025-09-09 11:50:08 +0200

    Add copy branch name keymap

--- nvim/lua/sopkk/keymaps.lua
+++ nvim/lua/sopkk/keymaps.lua
@@ -90,6 +90,10 @@ local mappings = {
     local remote = vim.fn.system('git config --get remote.origin.url')
     vim.fn.setreg('+', remote)
   end },
+  {'n', '<Leader>cb', function()
+    local branch = vim.fn.system('git rev-parse --abbrev-ref HEAD')
+    vim.fn.setreg('+', branch)
+  end },
   {'n', '<leader>cdl', '<cmd>cd %:p:h<CR>'},
   {'n', '<leader>cdh', '<cmd>cd -<CR>'},
 
