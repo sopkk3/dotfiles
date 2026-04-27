ac9d89efd5dbb1045dd181a3fcb6da66bab04363
sopkk3 <jorge3anoro@gmail.com>
2026-04-27 12:54:28 +0200

    Leave CursorLine enabled
    Now CursorLine is just coloring the line number, no line highlighting
    directly, so leaving it on resolves autocmd issues with enable/disable
    and doesn't confuse with the active window

--- nvim/lua/sopkk/options.lua
+++ nvim/lua/sopkk/options.lua
@@ -70,18 +70,6 @@ vim.api.nvim_create_autocmd({ 'TextYankPost' }, {
   end,
 })
 
-local set_cursorline = function(event, value, pattern)
-  vim.api.nvim_create_autocmd(event, {
-    group = group,
-    pattern = pattern,
-    callback = function()
-      vim.opt_local.cursorline = value
-    end,
-  })
-end
-set_cursorline('WinLeave', false)
-set_cursorline('WinEnter', true)
-
 vim.api.nvim_create_autocmd({ 'BufEnter', 'CursorHold', 'FocusGained' }, {
   group = group,
   desc = 'git branch',
