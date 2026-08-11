217c0c5e3468ec8296158e6da33869111c473365
sopkk3 <jorge3anoro@gmail.com>
2026-08-11 12:27:11 +0200

    Update ftplugin fold set up for toml

--- nvim/after/ftplugin/toml.lua
+++ nvim/after/ftplugin/toml.lua
@@ -1,5 +1,14 @@
 vim.opt_local.commentstring = [[# %s]]
 
-vim.opt_local.foldmethod = 'marker'
-vim.opt_local.foldmarker = '-- {{,-- }}'
-vim.opt_local.foldlevel = 0
+local function apply_folds()
+  vim.opt_local.foldmethod = 'marker'
+  vim.opt_local.foldmarker = '-- {{,-- }}'
+  vim.opt_local.foldlevel = 0
+end
+
+apply_folds()
+
+vim.api.nvim_create_autocmd('BufWinEnter', {
+  buffer = 0,
+  callback = apply_folds,
+})
